65a82b77bcc2370536d117b8f2a319ca4fd261bc
amrit4ec <amrit4ec@gmail.com>
2025-03-24 16:55:22 +0530

    Staging workflow

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofileaction321"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-south-1"
+    region = "ap-south-1"
   }
 
   required_version = "~> 1.6.3"
